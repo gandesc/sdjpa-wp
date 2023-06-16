920ec99f21b4764a9d0f42b1c4ce21ae2d709803
gandesc <mihai.ocneanu@gmail.com>
2023-06-16 13:22:09 +0300

    created user entity and setup mapping

---
+++ src/main/java/guru/springframework/sdjpa/wp/domain/User.java
@@ -0,0 +1,45 @@
+package guru.springframework.sdjpa.wp.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "wp_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_login")
+    private String user;
+
+    @Column(name = "user_pass")
+    private String password;
+
+    @Column(name = "user_nicename")
+    private String nicename;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_url")
+    private String url;
+
+    @Column(name = "user_registered")
+    private Timestamp registered;
+
+    @Column(name = "user_activation_key")
+    private String activationKey;
+
+    @Column(name = "user_status")
+    private Integer status;
+
+    private String displayName;
+
+}
